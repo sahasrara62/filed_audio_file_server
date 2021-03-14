5b184e5c55f1eb498151710ba3fbb55bc81a5606
sahasrara62 <nazi1062>
2021-03-14 23:47:05 +0530

    defining the podcast audio type server

---
+++ application/audio/podcast.py
@@ -0,0 +1,8 @@
+from application.audio import BaseAudio
+from application.databases import db, Column
+
+class Podcast(BaseAudio):
+	__tablename__ = "podcast"
+	name = Column(db.String(100), nullable=False)
+	host = Column(db.String(100), nullable=False)
+	participents = Column(db.Text(10), nullable=False)
