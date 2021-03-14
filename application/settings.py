a53dd19fbf77686f37a4e82acacc4fe2b52403ea
sahasrara62 <nazi1062>
2021-03-14 23:47:37 +0530

    setting file to define env

---
+++ application/settings.py
@@ -0,0 +1,10 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+ENV = env.str("FLASK_ENV", default="production")
+DEBUG = ENV == "development"
+SQLALCHEMY_DATABASE_URI = env.str("DATABASE_URL")
+SECRET_KEY = env.str("SECRET_KEY")
+SQLALCHEMY_TRACK_MODIFICATIONS = env.bool("SQLALCHEMY_TRACK_MODIFICATIONS", True)
