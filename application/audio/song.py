2ce3d30915a146db8071d9ee213704f795775bb0
sahasrara62 <nazi1062>
2021-03-14 23:47:54 +0530

    define song model defination

---
+++ application/audio/song.py
@@ -0,0 +1,7 @@
+from application.audio import BaseAudio
+from application.databases import db, Column
+
+class Song(BaseAudio):
+	__tablename__ = "song"
+	name = Column(db.String(100), nullable=False)
+
