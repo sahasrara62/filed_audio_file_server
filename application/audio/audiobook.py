33bf15e71d0691730ce51b4bb53d62e3d309e71a
sahasrara62 <nazi1062>
2021-03-14 21:55:09 +0530

    defining audio book model here

---
+++ application/audio/audiobook.py
@@ -0,0 +1,9 @@
+from application.audio import BaseAudio
+from application.databases import db, Column
+
+class AudioBook(BaseAudio):
+	__tablename__ = "audiobook"
+	title = Column(db.String(100), nullable=False)
+	author = Column(db.String(100), nullable=False)
+	narrator = Column(db.String(100), nullable=False)
+
