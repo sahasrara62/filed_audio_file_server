f45b8ae101d514028aebd7c198a6a689ae43603c
sahasrara62 <nazi1062>
2021-03-14 21:54:40 +0530

    base audio file , used in all other audio file models as base model

---
+++ application/audio/__init__.py
@@ -0,0 +1,20 @@
+from application.databases import db, Model, SurrogatePK, db, Column
+from datetime import datetime
+
+class BaseAudio(SurrogatePK, Model):
+	__abstract__ = True
+	uploaded_time = Column(db.DateTime, default=db.func.now(), nullable=False, onupdate=datetime.utcnow())
+	duration_time = Column(db.Integer, default=0, nullable=False)
+
+
+from .song import Song
+from .audiobook import AudioBook
+from .podcast import Podcast
+
+
+audiofiletype = {
+	"song": Song,
+	"audiobook": AudioBook,
+	"podcast": Podcast
+	
+}
